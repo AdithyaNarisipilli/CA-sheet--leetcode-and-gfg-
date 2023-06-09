73fb72a933a6a9538478fcf8d383c9114f7f6238
AdithyaNarisipilli <134121651+AdithyaNarisipilli@users.noreply.github.com>
2023-06-09 20:44:07 +0530

    Added solution - LeetHub

--- Find Kth permutation - GFG/find-kth-permutation.cpp
+++ Find Kth permutation - GFG/find-kth-permutation.cpp
@@ -13,26 +13,16 @@ class Solution
 public:
     string kthPermutation(int n, int k)
     {
-        int fact = 1; 
-        vector<int> numbers; 
-        for(int i = 1;i<n;i++) {
-            fact = fact * i; 
-            numbers.push_back(i); 
+        string a="";
+        for(int i=1;i<=n;i++){
+            a+=char('0'+i);
         }
-        numbers.push_back(n); 
-        string ans = ""; 
-        k = k - 1; 
-        while(true) {
-            ans = ans + to_string(numbers[k / fact]); 
-            numbers.erase(numbers.begin() + k / fact); 
-            if(numbers.size() == 0) {
-                break; 
-            }
-            
-            k = k % fact; 
-            fact = fact / numbers.size();
+        k--;
+        while(k--){
+            next_permutation(a.begin(),a.end());
         }
-        return ans; 
+        return a;
+    
     }
 
         
