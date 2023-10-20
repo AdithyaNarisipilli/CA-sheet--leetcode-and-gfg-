4f59faef50d99b9c11a4ac6c446c3749da13a6e6
Adithya Narisipilli <134121651+adithyanarisipilli@users.noreply.github.com>
2023-10-20 12:16:35 +0530

    Updated Solution - GfG to GitHub

--- Easy/Longest Distinct characters in string/longest-distinct-characters-in-string.cpp
+++ Easy/Longest Distinct characters in string/longest-distinct-characters-in-string.cpp
@@ -20,19 +20,22 @@ int main()
 
 int longestSubstrDistinctChars (string S)
 {
-    vector < int > mpp(256, -1);
-
-      int left = 0, right = 0;
-      int n = S.size();
-      int len = 0;
-      while (right < n) {
-        if (mpp[S[right]] != -1)
-          left = max(mpp[S[right]] + 1, left);
-
-        mpp[S[right]] = right;
-
-        len = max(len, right - left + 1);
-        right++;
-      }
-      return len;
+    int maxi=1;
+    int n=S.length();
+    unordered_map<char,int>mpp;
+    int len=1;
+    mpp[S[0]]=0;
+    for(int i=1;i<n;i++){
+        if(mpp.find(S[i])==mpp.end()){
+            len++;
+            maxi=max(maxi,len);
+            mpp[S[i]]=i;
+        }
+        else{
+            len=0;
+            i=mpp[S[i]];
+            mpp.clear();
+        }
+    }
+    return maxi;
 }
